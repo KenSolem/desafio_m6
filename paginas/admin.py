74278d27b721fb5df059fcfd37414ae5013d2031
Gonzalo Lemus <161665431+KenSolem@users.noreply.github.com>
2024-09-22 23:00:12 -0300

    se crea modelo flan y se agrega 8 fflanes

--- paginas/admin.py
+++ paginas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from paginas.models import Flan
 
-# Register your models here.
+
+class FlanAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Flan, FlanAdmin)
